7f81d663bf9f7a36dd62fec4783d15068ec2581d
Аліхан Бекіров <alikhanbekirov@gmail.com>
2024-04-14 14:44:25 +0300

    task_02 done

--- lab_01/Lab_01/Task_01/Program.cs
+++ lab_01/Lab_01/Task_01/Program.cs
@@ -1,152 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using classes;
 
-// Клас для грошей
-public class Money
+namespace Task_01
 {
-    public int Dollars { get; set; } // Ціла частина
-    public int Cents { get; set; }   // Дробова частина
-
-    // Конструктор
-    public Money(int dollars, int cents)
+    class Program
     {
-        Dollars = dollars;
-        Cents = cents;
-    }
-
-    // Метод для виведення суми на екран
-    public void DisplayAmount()
-    {
-        Console.WriteLine($"Сума: {Dollars} грн {Cents} коп");
-    }
-}
-
-// Клас для продукту
-public class Product
-{
-    public string Name { get; set; }
-    public Money Price { get; set; }
-
-    // Конструктор
-    public Product(string name, Money price)
-    {
-        Name = name;
-        Price = price;
-    }
-
-    // Метод для зменшення ціни
-    public void DecreasePrice(int amount)
-    {
-        Price.Cents -= amount;
-        if (Price.Cents < 0)
+        static void Main(string[] args)
         {
-            Price.Dollars--;
-            Price.Cents += 100;
-        }
-    }
-}
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            // Створюємо гроші
+            Money price1 = new Money(10, 50);
+            Money price2 = new Money(5, 75);
 
-// Клас для товарного складу
-public class Warehouse
-{
-    public string Name { get; set; }
-    public string Unit { get; set; }
-    public Money UnitPrice { get; set; }
-    public int Quantity { get; set; }
-    public DateTime LastStockDate { get; set; }
+            // Створюємо товари
+            Product product1 = new Product("Яблуко", price1);
+            Product product2 = new Product("Банан", price2);
 
-    // Конструктор
-    public Warehouse(string name, string unit, Money unitPrice, int quantity, DateTime lastStockDate)
-    {
-        Name = name;
-        Unit = unit;
-        UnitPrice = unitPrice;
-        Quantity = quantity;
-        LastStockDate = lastStockDate;
-    }
-}
-// Клас для звітності
-public class Reporting
-{
-    private List<Warehouse> warehouseItems;
+            // Відображаємо інформацію про товари
+            Console.WriteLine($"Назва: {product1.Name}, Ціна: {product1.Price.Dollars} грн {product1.Price.Cents} коп");
+            Console.WriteLine($"Назва: {product2.Name}, Ціна: {product2.Price.Dollars} грн {product2.Price.Cents} коп");
 
-    // Конструктор
-    public Reporting()
-    {
-        warehouseItems = new List<Warehouse>();
-    }
+            // Зменшуємо ціну першого товару
+            product1.DecreasePrice(2);
 
-    // Метод для реєстрації надходження товару
-    public void RegisterIncome(Warehouse item)
-    {
-        warehouseItems.Add(item);
-        Console.WriteLine($"Зареєстровано надходження {item.Quantity} {item.Unit}(ів) товару {item.Name}.");
-    }
+            // Відображаємо оновлену ціну першого товару
+            Console.WriteLine($"Нова ціна для {product1.Name}: {product1.Price.Dollars} грн {product1.Price.Cents} коп");
 
-    // Метод для відвантаження товару
-    public void ShipItem(Warehouse item)
-    {
-        if (warehouseItems.Contains(item))
-        {
-            warehouseItems.Remove(item);
-            Console.WriteLine($"Відвантажено {item.Quantity} {item.Unit}(ів) товару {item.Name}.");
-        }
-        else
-        {
-            Console.WriteLine("Товар не знайдено на складі.");
+            // Створюємо товарний склад
+            Money unitPrice = new Money(2, 0);
+            Warehouse warehouseItem = new Warehouse("Яблука", "кг", unitPrice, 100, DateTime.Now);
+
+            // Відображаємо інформацію про товарний склад
+            Console.WriteLine($"Товарний склад: {warehouseItem.Name}, Одиниця виміру: {warehouseItem.Unit}, Кількість: {warehouseItem.Quantity}");
+
+            // Створюємо екземпляр класу звітності
+            Reporting reporting = new Reporting();
+
+            // Реєстрація надходження товару на склад
+            reporting.RegisterIncome(warehouseItem);
+
+            // Відображення звіту по інвентаризації
+            reporting.InventoryReport();
+
+            // Відвантаження товару зі складу
+            reporting.ShipItem(warehouseItem);
+
+            // Відображення звіту по інвентаризації
+            reporting.InventoryReport();
         }
     }
 
-    // Метод для звіту по інвентаризації
-    public void InventoryReport()
-    {
-        Console.WriteLine("Звіт по інвентаризації:");
-        if (warehouseItems.Count == 0)
-        {
-            Console.WriteLine("На складі немає товарів.");
-        }
-        else
-        {
-            foreach (var item in warehouseItems)
-            {
-                Console.WriteLine($"- {item.Name}: {item.Quantity} {item.Unit}(ів)");
-            }
-        }
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.OutputEncoding = Encoding.Unicode;
-        Console.InputEncoding = Encoding.Unicode;
-        // Приклад використання класів і методів
-        Money price1 = new Money(10, 50);
-        Product product1 = new Product("Яблуко", price1);
-
-        Money price2 = new Money(5, 75);
-        Product product2 = new Product("Банан", price2);
-
-        Money unitPrice = new Money(2, 0);
-        Warehouse warehouseItem = new Warehouse("Яблука", "кг", unitPrice, 100, DateTime.Now);
-
-        Reporting reporting = new Reporting();
-
-        // Додамо товари до складу
-        reporting.RegisterIncome(warehouseItem);
-
-        // Реєстрація надходження товару
-        reporting.RegisterIncome(warehouseItem);
-
-        // Відвантаження товару
-        reporting.ShipItem(warehouseItem);
-
-        // Звіт по інвентаризації
-        reporting.InventoryReport();
-
-        // Вивід суми
-        price1.DisplayAmount();
-    }
 }
---
+++ lab_01/Lab_01/Task_01/class.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Клас для грошей
+namespace classes
+{
+    public class Money
+    {
+        public int Dollars { get; set; } // Ціла частина
+        public int Cents { get; set; }   // Дробова частина
+
+        // Конструктор
+        public Money(int dollars, int cents)
+        {
+            Dollars = dollars;
+            Cents = cents;
+        }
+
+        // Метод для виведення суми на екран
+        public void DisplayAmount()
+        {
+            Console.WriteLine($"Сума: {Dollars} грн {Cents} коп");
+        }
+    }
+
+    // Клас для продукту
+    public class Product
+    {
+        public string Name { get; set; }
+        public Money Price { get; set; }
+
+        // Конструктор
+        public Product(string name, Money price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        // Метод для зменшення ціни
+        public void DecreasePrice(int amount)
+        {
+            Price.Cents -= amount;
+            if (Price.Cents < 0)
+            {
+                Price.Dollars--;
+                Price.Cents += 100;
+            }
+        }
+    }
+
+
+    public class Warehouse
+    {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public Money UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public DateTime LastStockDate { get; set; }
+
+
+        public Warehouse(string name, string unit, Money unitPrice, int quantity, DateTime lastStockDate)
+        {
+            Name = name;
+            Unit = unit;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+            LastStockDate = lastStockDate;
+        }
+    }
+
+    public class Reporting
+    {
+        private List<Warehouse> warehouseItems;
+
+
+        public Reporting()
+        {
+            warehouseItems = new List<Warehouse>();
+        }
+
+
+        public void RegisterIncome(Warehouse item)
+        {
+            warehouseItems.Add(item);
+            Console.WriteLine($"Зареєстровано надходження {item.Quantity} {item.Unit}(ів) товару {item.Name}.");
+        }
+
+
+        public void ShipItem(Warehouse item)
+        {
+            if (warehouseItems.Contains(item))
+            {
+                warehouseItems.Remove(item);
+                Console.WriteLine($"Відвантажено {item.Quantity} {item.Unit}(ів) товару {item.Name}.");
+            }
+            else
+            {
+                Console.WriteLine("Товар не знайдено на складі.");
+            }
+        }
+
+
+        public void InventoryReport()
+        {
+            Console.WriteLine("Звіт по інвентаризації:");
+            if (warehouseItems.Count == 0)
+            {
+                Console.WriteLine("На складі немає товарів.");
+            }
+            else
+            {
+                foreach (var item in warehouseItems)
+                {
+                    Console.WriteLine($"- {item.Name}: {item.Quantity} {item.Unit}(ів)");
+                }
+            }
+        }
+    }
+}
